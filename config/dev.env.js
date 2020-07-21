f1a3da8ced402bded408322444f4eae39e5352d0
hul <hul>
2020-07-21 15:43:32 +0800

    工作心得

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,12 +5,13 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-  // BASE_API: '"http://192.168.31.251:9000"', // 吴 xiaomi  
-  // BASE_API: '"http://192.168.101.13:9000"', // 谢 5g  
-  // BASE_API: '"http://47.114.32.158:80"', // 本地服务器s
+  // BASE_API: '"http://192.168.31.251:9000/"', // 吴 xiaomi  
+  // BASE_API: '"http://192.168.101.13:9000/"', // 谢 5g  
+  // BASE_API: '"http://47.114.32.158:80/"', // 本地服务器s
   // BASE_API: '"https://47.111.161.50:9000/"', // 测试服务器
-  // BASE_API: '"http://api-test.hooskp.com"', // 测试服务器http
-  BASE_API: '"https://api-test.hooskp.com:9100"', // 测试服务器https
+  // BASE_API: '"http://api-test.hooskp.com/"', // 测试服务器http
+  // BASE_API: '"https://api-test.hooskp.com:9100/"', // 测试服务器https
+  BASE_API: '"https://api.hooskp.com/"', // 正式服务器
 
 
   // 二维码招生推广
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://api.org.zhihuiqintong.com/"',
+  BASE_API: '"http://api.hooskp.com/"',
   // 二维码招生推广
   QRCODE_URL:'"http://other.zhihuiqintong.com/org/qrcode/index?key="',
   // 腾讯云cos直传参数获取
